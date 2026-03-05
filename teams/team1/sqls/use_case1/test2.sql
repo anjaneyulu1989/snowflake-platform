18ab3bc03d01a1a0d34013146bb6486effc377ed
YARRAPOA <yarrapoa@wwt.com>
2026-03-05 19:16:55 +0000

    test

--- teams/team1/sqls/use_case1/test2.sql
+++ teams/team1/sqls/use_case1/test2.sql
@@ -4,4 +4,5 @@ VALUES
     ('sarah_miller', 'sarah.miller@example.com'),
     ('david_chen', 'david.chen@example.com'),
     ('john_doe', 'john.doe@example.com'),
+    ('john_doe1', 'john.doe1@example.com'),
     ('emma_wilson', 'emma.wilson@example.com');
